00e1aee39f705c4d44f52379b3edbb4422bf47d0
Alexian Hentiu <alexianhentiu1208@gmail.com>
2024-11-05 20:30:37 +0200

    change sql tables

--- app/auth.py
+++ app/auth.py
@@ -15,7 +15,7 @@ def home():
     return jsonify({"message": "Hello, Flask!"})
 
 @auth_bp.route('/users')
-def get_posts():
+def get_users():
     users = User.query.all()
     return {'users': [user.email for user in users]}
 
@@ -28,10 +28,20 @@ def register():
         first_name=data.get('first_name'),
         last_name=data.get('last_name')
     )
-    db.session.add(new_user)
-    db.session.commit()
 
-    return jsonify({"message": "User registered successfully"}), 201
+    existing_user = User.query.filter_by(email=new_user.email).first()
+    if existing_user:
+        return jsonify({"message": "Email already in use"}), 400
+
+    try:
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({"message": "User registered successfully"}), 201
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"message": "User registration failed", "error": str(e)}), 400
+
+
 
 @auth_bp.route('/login', methods=['POST'])
 def login():
--- app/models.py
+++ app/models.py
@@ -1,25 +1,26 @@
-from sqlalchemy import Integer, VARCHAR, ForeignKey
+from sqlalchemy import Integer, VARCHAR, Text, ForeignKey
 from sqlalchemy.orm import mapped_column
 from flask_login import UserMixin
 from app import db
 
 class User(db.Model, UserMixin):
-    id = mapped_column(Integer, primary_key=True, autoincrement=True, nullable=False)
-    email = mapped_column(VARCHAR(255), unique=True, nullable=False)
-    hashed_password = mapped_column(VARCHAR(255), unique=False, nullable=False)
-    first_name = mapped_column(VARCHAR(255), unique=False, nullable=True)
-    last_name = mapped_column(VARCHAR(255), unique=False, nullable=True)
+    id = mapped_column(Integer, primary_key=True, autoincrement=True)
+    email = mapped_column(VARCHAR(255), unique=True)
+    hashed_password = mapped_column(VARCHAR(255) )
+    first_name = mapped_column(VARCHAR(255), nullable=True)
+    last_name = mapped_column(VARCHAR(255), nullable=True)
     
     def __repr__(self):
         return f'<User {self.email}>'
 
 class VaultEntry(db.Model):
-    id = mapped_column(Integer, primary_key=True, autoincrement=True, nullable=False)
-    user_id = mapped_column(Integer, ForeignKey('user.id'), unique=False, nullable=False)
-    title = mapped_column(VARCHAR(255), unique=False, nullable=False)
-    url = mapped_column(VARCHAR(255), unique=False, nullable=True)
-    encrypted_username = mapped_column(VARCHAR(255), unique=False, nullable=False)
-    encrypted_password = mapped_column(VARCHAR(255), unique=False, nullable=False)
+    id = mapped_column(Integer, primary_key=True, autoincrement=True)
+    user_id = mapped_column(Integer, ForeignKey('user.id'))
+    title = mapped_column(VARCHAR(255))
+    url = mapped_column(VARCHAR(255), nullable=True)
+    encrypted_username = mapped_column(VARCHAR(255), nullable=True)
+    encrypted_password = mapped_column(VARCHAR(255), nullable=True)
+    notes = mapped_column(Text, nullable=True)
 
     def to_dict(self):
         return {
@@ -27,7 +28,8 @@ class VaultEntry(db.Model):
             'title': self.title,
             'url': self.url,
             'encrypted_username': self.encrypted_username,
-            'encrypted_password': self.encrypted_password
+            'encrypted_password': self.encrypted_password,
+            'notes': self.notes
         }
 
     def __repr__(self):
